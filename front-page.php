348d8818025a2549120574cf581f23b553ce6780
Krypt0niT <k269692@gmail.com>
2022-10-09 15:27:29 +0200

    zmena nazvu

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 <div id="hero">
     <div class="container d-flex align-items-center justify-content-center h-100">
 
-        <h1 class="display-2 fw-bold">Vitajte na mojom e-shope</h1>
+        <h1 class="display-2 fw-bold ">Vitajte na mojom e-shope</h1>
     </div>
 </div>
 
